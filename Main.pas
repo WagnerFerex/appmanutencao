a1bb744233d92070686ad13a24e17cde8596d14c
Wagner Vasconcelos <wagnerferex@gmail.com>
2021-11-28 17:49:02 -0300

    [Implementação 1]
    implementando um gerenciador de exceções ao projeto.

--- Main.pas
+++ Main.pas
@@ -7,6 +7,15 @@ uses
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
+  TException = class
+  private
+    FLogFile : String;
+  public
+    constructor Create;
+    procedure TrataException(Sender: TObject; E : Exception);
+    procedure GravarLog(Value : String);
+  end;
+
   TfMain = class(TForm)
     btDatasetLoop: TButton;
     btThreads: TButton;
@@ -19,11 +28,12 @@ type
 
 var
   fMain: TfMain;
+  Exception1 : TException;
 
 implementation
 
 uses
-  DatasetLoop, ClienteServidor;
+  DatasetLoop, ClienteServidor, System.UITypes;
 
 {$R *.dfm}
 
@@ -36,5 +46,53 @@ procedure TfMain.btStreamsClick(Sender: TObject);
 begin
   fClienteServidor.Show;
 end;
+
+{ TException }
+
+constructor TException.Create;
+begin
+  FLogFile := ChangeFileExt(ParamStr(0), '.log');
+  Application.OnException := TrataException;
+end;
+
+procedure TException.GravarLog(Value: String);
+var
+  txtLog : TextFile;
+begin
+  AssignFile(txtLog, FLogFile);
+  if FileExists(FLogFile) then
+    Append(txtLog)
+  else
+    Rewrite(txtLog);
+  Writeln(txtLog, FormatDateTime('dd/mm/YY hh:nn:ss - ', Now) + Value);
+  CloseFile(txtLog);
+end;
+
+procedure TException.TrataException(Sender: TObject; E: Exception);
+begin
+  GravarLog('===========================================');
+  if TComponent(Sender) is TForm then
+  begin
+    GravarLog('Form: '    + TForm(Sender).Name);
+    GravarLog('Caption: ' + TForm(Sender).Caption);
+    GravarLog('Erro:'     + E.ClassName);
+    GravarLog('Erro:'     + E.Message);
+  end
+  else
+  begin
+    GravarLog('Form: ' + TForm(TComponent(Sender).Owner).Name);
+    GravarLog('Caption: ' + TForm(TComponent(Sender).Owner).Caption);
+    GravarLog('Erro:' + E.ClassName);
+    GravarLog('Erro:' + E.Message);
+  end;
+
+  Vcl.Dialogs.MessageDlg('Erro: '+ E.Message, mtError, [mbOk], 0);
+end;
+
+initialization
+  Exception1 := TException.Create;
+
+finalization
+  Exception1.Free;
 
 end.
